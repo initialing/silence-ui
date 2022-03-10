0cda7c26c04be5267e106825dd5fb725bd5afe43
scholes <zqt_scholes@163.com>
2022-03-10 21:19:50 +0800

    achieve to effect of element Carousel

--- packages/components/carousel/src/carousel.vue
+++ packages/components/carousel/src/carousel.vue
@@ -62,7 +62,8 @@ export default defineComponent({
                 carousel.getElementsByClassName("si-carousel-item");
             const carouselWidth = carousel.clientWidth;
             const carouselHeight = carousel.clientHeight;
-            boxWidth.value = carouselWidth * carouselItems.length;
+            const length = carouselItems.length;
+            boxWidth.value = carouselWidth * length;
             const carouselInner: HTMLElement = carousel.getElementsByClassName(
                 "si-carousel__inner"
             )[0] as HTMLElement;
@@ -70,11 +71,50 @@ export default defineComponent({
             carouselInner.style.height = carouselHeight + "px";
             carousel.style.overflow = "hidden";
             carousel.style.float = "left";
-            for (let i = 0; i < carouselItems.length; i++) {
+            for (let i = 0; i < length; i++) {
                 (carouselItems[i] as HTMLElement).style.width =
                     carouselWidth + "px";
                 (carouselItems[i] as HTMLElement).style.float = "left";
             }
+            let i = 0;
+            setInterval(() => {
+                let current = i % length;
+                let next = (i + 1) % length;
+                let currentElm: HTMLElement = carouselItems[
+                    current
+                ] as HTMLElement;
+                let nextElm: HTMLElement = carouselItems[next] as HTMLElement;
+                currentElm.style.transition = "transform .8s";
+                currentElm.style.transform = `translateX(${
+                    0 - (current + 1) * carouselWidth
+                }px)`;
+                nextElm.style.transition = "transform .8s";
+                nextElm.style.transform = `translateX(${
+                    0 - next * carouselWidth
+                }px)`;
+                i++;
+                setTimeout(() => {
+                    currentElm.style.transition = "";
+                    nextElm.style.transition = "";
+                    for (let j = 0; j < length; j++) {
+                        if (j === i % length) continue;
+                        if (j < i % length) {
+                            (
+                                carouselItems[j] as HTMLElement
+                            ).style.transform = `translateX(${
+                                (length - j - (i % length)) * carouselWidth
+                            }px)`;
+                        } else {
+                            (
+                                carouselItems[j] as HTMLElement
+                            ).style.transform = `translateX(${
+                                0 - (i % length) * carouselWidth
+                            }px)`;
+                        }
+                    }
+                    i %= length;
+                }, 800);
+            }, 2000);
         });
         return {
             carouselId,
